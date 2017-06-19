a5db15742b7936641bcebc5afcc87c7681f0680b
Carlos Velasco <carlos@2activedesign.com>
2017-06-19 21:49:09 +0200

    on click tests

--- js/master.js
+++ js/master.js
@@ -1,23 +1,48 @@
+require('./babylon.min.js');
+//
 const BABYLON = window.BABYLON;
 //
-if (BABYLON.Engine.isSupported()) {
-  window.canvas = document.getElementById('renderCanvas');
-  window.engine = new BABYLON.Engine(window.canvas, true);
-  BABYLON.SceneLoader.Load('', './js/ballistic.babylon', window.engine, (
-    scene) => {
-    // Wait for textures and shaders to be ready
-    scene.executeWhenReady(() => {
-      // Attach camera to canvas inputs
-      scene.activeCamera.attachControl(window.canvas);
-      // scene.enablePhysics(new BABYLON.Vector3(0, -9.81, 0), new BABYLON
-      //   .OimoJSPlugin());
-      // Once the scene is loaded, just register a render loop to render it
-      window.engine.runRenderLoop(() => {
-        scene.render();
-      });
+const canvas = document.getElementById('renderCanvas');
+const engine = new BABYLON.Engine(canvas, true);
+//
+BABYLON.SceneLoader.Load('', './js/ballistic.babylon', engine, (scene) => {
+  scene.executeWhenReady(() => {
+    window.scene = scene;
+    const greenMesh = scene.getMeshByID('greenCube');
+    const redMesh = scene.getMeshByID('redCube');
+    const greenCube = scene._physicsEngine._impostors[0];
+    const redCube = scene._physicsEngine._impostors[1];
+    redCube.setLinearVelocity(new BABYLON.Vector3(0, 1, 0));
+    redCube.setAngularVelocity(new BABYLON.Quaternion(10, 0, 0, 0));
+    greenCube.applyImpulse(new BABYLON.Vector3(0, 15, 0),
+      greenMesh.getAbsolutePosition());
+    greenMesh.actionManager = new BABYLON.ActionManager(scene);
+    redMesh.actionManager = new BABYLON.ActionManager(scene);
+    //
+    greenMesh.actionManager.registerAction(new BABYLON.ExecuteCodeAction({
+      trigger: BABYLON.ActionManager.OnPickTrigger,
+      parameter: greenMesh,
+    }, () => {
+      greenCube.applyImpulse(new BABYLON.Vector3(0, 15, 0),
+        greenMesh.getAbsolutePosition());
+    }));
+    //
+    //
+    redMesh.actionManager.registerAction(new BABYLON.ExecuteCodeAction({
+      trigger: BABYLON.ActionManager.OnPickTrigger,
+      parameter: redMesh,
+    }, () => {
+      redCube.applyImpulse(new BABYLON.Vector3(0, 15, 0),
+        redMesh.getAbsolutePosition());
+    }));
+    //
+    scene.activeCamera.attachControl(canvas);
+    engine.runRenderLoop(() => {
+      scene.render();
     });
   });
-}
+  return scene;
+});
 window.addEventListener('resize', () => {
-  window.engine.resize();
+  engine.resize();
 });
